f9a1701ef8a7ddeb21a70b14ee6de5f1dde5d49b
Coding-Zuo <zzuuoo666@sina.com>
2021-01-23 23:38:17 +0800

    2020-1-23

---
+++ Leetcode/栈队/20有效的括号.cpp
@@ -0,0 +1,48 @@
+//
+// Created by 左玉晖 on 2020/11/21.
+// https://leetcode-cn.com/problems/valid-parentheses/
+#include <iostream>
+#include<algorithm>
+#include<vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stackStr;
+        for (char c:s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stackStr.push(c);
+            } else {
+                if (stackStr.empty()) {
+                    return false;
+                }
+                char cs = stackStr.top();
+                if (c == ']' && cs == '[') stackStr.pop();
+                else if (c == '}' && cs == '{') stackStr.pop();
+                else if (c == ')' && cs == '(') stackStr.pop();
+                else return false;
+            }
+        }
+        if (!stackStr.empty()) return false;
+        return true;
+    }
+};
+
+int main(int argc, const char *argv[]) {
+    Solution sl;
+    vector<int> arr1 = {2, 3};
+    vector<int> arr2 = {3, 7};
+    vector<int> arr3 = {4, 3};
+    vector<int> arr4 = {3, 7};
+    sl.isValid("{}");
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/20.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "20"
+  "20.pdb"
+  "CMakeFiles/20.dir/Leetcode/栈队/20有效的括号.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
