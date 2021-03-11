83e91165a124464bb3c425d7b1ed8d2d8c401c8e
Coding-Zuo <zzuuoo666@sina.com>
2021-03-11 10:13:12 +0800

    2021.3.11

---
+++ Leetcode/DP/96不同的二叉搜索树.cpp
@@ -0,0 +1,69 @@
+//
+// Created by 左玉晖 on 2020/11/21.
+// https://leetcode-cn.com/problems/unique-binary-search-trees/
+#include <iostream>
+#include<algorithm>
+#include<vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <set>
+#include <iomanip>
+
+using namespace std;
+
+/**
+ * 确定dp数组以及下标含义
+ * dp[3]，就是 元素1为头结点搜索树的数量 + 元素2为头结点搜索树的数量 + 元素3为头结点搜索树的数量
+ * dp[i] ：1到i为节点组成的二叉搜索树的个数为dp[i]。
+ *
+ * 确定递推公式
+ * 在上面的分析中，其实已经看出其递推关系， dp[i] += dp[以j为头结点左子树节点数量] * dp[以j为头结点右子树节点数量]
+
+j相当于是头结点的元素，从1遍历到i为止。
+
+所以递推公式：dp[i] += dp[j - 1] * dp[i - j]; ，j-1 为j为头结点左子树节点数量，i-j 为以j为头结点右子树节点数量
+ *
+ * dp初始条件
+ *
+ * 计算顺序
+ * 顺序
+ *
+ * 举例推导dp数组
+ *
+ *
+ * 答案是
+ *
+ */
+#define INT_MAX 0x7fffffff
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
+
+
+int main(int argc, const char *argv[]) {
+    Solution sl;
+    int a[3] = {1, 3, 1};
+    int b[3] = {1, 5, 1};
+    int c[3] = {4, 2, 1};
+    vector<int> nums(a, a + sizeof(a) / sizeof(int));
+    vector<int> nums1(b, b + sizeof(b) / sizeof(int));
+    vector<int> nums2(c, c + sizeof(c) / sizeof(int));
+    vector<vector<int>> grid;
+    grid.push_back(nums);
+    grid.push_back(nums1);
+    grid.push_back(nums2);
+
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/96.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "96"
+  "96.pdb"
+  "CMakeFiles/96.dir/Leetcode/DP/96不同的二叉搜索树.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/96.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
