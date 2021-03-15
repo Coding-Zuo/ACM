b373c8f65369f91063f212da194756ef13b92d1c
Coding-Zuo <zzuuoo666@sina.com>
2021-03-15 11:38:16 +0800

    并查集学习1

--- Leetcode/马士兵/并查集947移除最多的同行或同列石头.cpp
+++ Leetcode/马士兵/并查集947移除最多的同行或同列石头.cpp
@@ -14,92 +14,100 @@ using namespace std;
 
 #define INT_MAX 0x7fffffff
 
-class Djset {
+//并查集特殊模板2
+class UnionFind {
+    //本题因为是按行列进行查找，优化可以直接采用哈希表
 public:
-    vector<int> parent; //记录节点的根
-    vector<int> rank; //记录根节点的深度用于优化
-    Djset(int n) : parent(vector<int>(n)), rank(vector<int>(n)) {
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+    // 哈希表映射关系[key,parent]
+    unordered_map<int, int> parent;
+    // 总共有多少不连通的并查集
+    int count = 0;
+
+    //并查集查找首领节点操作
+    int Find(int index) {
+        //一开始构建并查集时，假如key值不在并查集中则构建哈希表映射，count++
+        if (parent.find(index) == parent.end()) {
+            parent[index] = index;
+            count++;
         }
+        //查找并查集的首领节点并优化
+        if (parent[index] != index) {
+            parent[index] = Find(parent[index]);
+        }
+        return parent[index];
     }
 
-    int find(int x) {
-        //压缩方式：直接指向根节点
-        if (x != parent[x]) {
-            parent[x] = find(parent[x]);
+    // 并查集合并操作
+    void Uniod(int index1, int index2) {
+        int parent1 = Find(index1);
+        int parent2 = Find(index2);
+        // 直接把两个首领一样的结果返回过滤，否则会让count多减1
+        if (parent1 == parent2) {
+            return;
         }
-        return parent[x];
+        parent[parent1] = parent2;
+        count--;
     }
 
-    void merge(int x, int y) {
-        int rootx = find(x);
-        int rooty = find(y);
-        if (rootx != rooty) {
-            //按智合并
-            if (rank[rootx] < rank[rooty]) {
-                swap(rootx, rooty);
-            }
-            parent[rooty] = rootx;
-            if (rank[rootx] == rank[rooty]) rank[rootx] += 1;
+    // 思路：将石头行列的数值构建并查集，因此行或列需要加10001区分开
+    // 合并的意思所有横坐标为 x 的石头和所有纵坐标为 y 的石头都属于同一个并查集
+    // 最后返回石头数量 - 并查集的数量，就是题目要求的最多
+    int removeStones(vector<vector<int>> &stones) {
+        UnionFind uf;
+        for (auto &stone:stones) {
+            uf.Uniod(stone[0] + 10001, stone[1]);
         }
+        return stones.size() - uf.count;
     }
 };
 
-class UF {
-public:
-    UF(int n);
 
-    bool connected(int w, int v); // 判断是否连通
-    void Union(int w, int v); // 合并两个集合,大写首字母为了区别关键字
-    int find(int p);  //找到p所在集合的根
-
+class Solution {
 private:
-    vector<int> *m_parent = nullptr; // parent[i] = parent of i
-    vector<int> *m_rank = nullptr; //rank[i] = rank of subtree at i
-    int m_count = 0; // number of components
-    void validate(int p);
+    int n = 20005;
+    int father[20005];
+
+    //并查集初始化
+    void init() {
+        for (int i = 0; i < n; i++) {
+            father[i] = i;
+        }
+    }
+
+    //并查集里寻根过程
+    int find(int u) {
+        return u == father[u] ? u : father[u] = find(father[u]);
+    }
+
+    // 将v->u 这条边加入并查集
+    void join(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u == v) return;
+        father[v] = u;
+    }
+
+    //判断u和v是否找到同一个根
+    bool same(int u, int v) {
+        u = find(u);
+        v = find(v);
+        return u == v;
+    }
+
+public:
+    int removeStones(vector<vector<int>> &stones) {
+        init();
+        for (int i = 0; i < stones.size(); i++) {
+            join(stones[i][0], stones[i][1] + 10000);
+        }
+        unordered_map<int, bool> umap;
+        for (int i = 0; i < stones.size(); i++) {
+            umap[find(stones[i][0])] = true;
+            umap[find(stones[i][1] + 10000)] = true;
+        }
+        return stones.size() - umap.size();
+    }
 };
-
-UF::UF(int n) {
-    m_parent = new vector<int>(n);
-    m_rank = new vector<int>(n, 0);
-
-    for (int i = 0; i < n; i++) {
-        m_parent->at(i) = i;
-    }
-}
-
-bool UF::connected(int w, int v) {
-    return find(w) == find(v);
-}
-
-void UF::Union(int w, int v) {
-    int rootW = find(w);
-    int rootV = find(v);
-    if (rootV == rootW) return; //同一集合不用合并
-
-    //把rank理解为合并次数，次数越多，集合越大，并让小的衣服大的
-    if (m_rank->at(rootW) < m_rank->at(rootV)) {
-        m_parent->at(rootW) = rootV;
-    } else if (m_rank->at(rootW) > m_rank->at(rootV)) {
-        m_parent->at(rootV) = rootW;
-    } else {
-        m_parent->at(rootV) = rootW;
-        m_rank->at(rootW)++;
-    }
-    m_count--;
-}
-
-int UF::find(int p) {
-    while (p != m_parent->(p)) {
-        // 路径折半压缩
-        m_parent->at(p) = (*m_parent)[m_parent->at(p)];
-        p = m_parent->at(p);
-    }
-    return p;
-}
-
 
 int main(int argc, const char *argv[]) {
     Solution sl;
